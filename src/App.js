f12c77ff782ccfb0c8b844db04e5d2986cce9c89
Luca <lukkino31@gmail.com>
2022-11-04 11:11:45 +0100

    fetch ip adress, display ip on the screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getUserInfo } from './fetches/fetches';
+
 
 function App() {
+
+  const [ipInfo, setIpInfo] = useState({});
+
+  useEffect(() =>{
+
+    (async () => {
+      setIpInfo(await getUserInfo())
+  })();
+}, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      <h2>Your Ip address is {ipInfo.ip}</h2>
     </div>
   );
 }
